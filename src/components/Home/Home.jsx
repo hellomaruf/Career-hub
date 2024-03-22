754d92d20713346927aa470438046d40ed9f63f8
maruf-ahmed <marufahamed1110@gmail.com>
2024-03-23 01:55:03 +0600

    added hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,29 @@
-
 function Home() {
   return (
     <div>
-      <h2>HOME Section</h2>
+      <div className="hero bg-blue-50">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src="./images/user.png" className="max-w-2xl bottom-0" />
+          <div>
+            <h1 className="text-6xl font-bold">
+              One Step Closer To Your{" "}
+              <span className="bg-gradient-to-r from-blue-400  to-purple-400 inline-block text-transparent bg-clip-text">
+                Dream Job!
+              </span>
+            </h1>
+            <p className="py-6">
+              Explore thousands of job opportunities with all the information
+              you need. Its your future. Come find it. Manage all your job
+              application from start to finish.
+            </p>
+            <button className="btn bg-gradient-to-r from-blue-400 to-purple-400 text-white">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -46,8 +46,8 @@ function Nav() {
     </>
   );
   return (
-    <div>
-      <div className="navbar bg-base-100 max-w-7xl mx-auto">
+    <div className=" ">
+      <div className="navbar bg-base-100 max-w-7xl h-24 mx-auto py-6 ">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
