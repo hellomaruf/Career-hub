ac80846a39f6d7c96fdc40824c854aa0e70873fc
maruf-ahmed <marufahamed1110@gmail.com>
2024-03-23 13:58:52 +0600

    added show all functionality

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,17 +3,34 @@ import FearureJob from "../FeatureJob/FearureJob";
 
 function FeaturedJobs() {
   const [featuredJobs, setFeaturedJobs] = useState([]);
+  const [jobsLength, setJobsLength] = useState(4);
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
       .then((data) => setFeaturedJobs(data));
   }, []);
-  console.log(featuredJobs);
+
   return (
-    <div className="grid grid-cols-2 gap-6 max-w-7xl mx-auto pb-16">
-      {featuredJobs.map((items, index) => (
-        <FearureJob key={index} job={items} />
-      ))}
+    <div className="">
+      <div className="grid grid-cols-2 gap-6 max-w-7xl mx-auto pb-8">
+        {featuredJobs.slice(0, jobsLength).map((items, index) => (
+          <FearureJob key={index} job={items} />
+        ))}
+      </div>
+      <div
+        className={
+          jobsLength === featuredJobs.length
+            ? "hidden text-center"
+            : "text-center"
+        }
+      >
+        <button
+          onClick={() => setJobsLength(featuredJobs.length)}
+          className="btn bg-gradient-to-r from-blue-400 to-purple-400 text-white"
+        >
+          Show All
+        </button>
+      </div>
     </div>
   );
 }
